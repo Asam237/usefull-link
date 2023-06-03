62a01028800dcc9e79aef701b254611ed3bd75a6
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-06-03 05:37:33 +0100

    read jwt in .env file

--- api/src/utils/verifyToken.ts
+++ api/src/utils/verifyToken.ts
@@ -1,11 +1,12 @@
 import * as jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import { JWT_SECRET } from "../shared/core/config";
 
 export const verifyToken = (req: any, res: Response, next: NextFunction) => {
   const authHeader: any = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, "@&Cbhjs__2v+_)!@BED^", (error: any, user: any) => {
+    jwt.verify(token, JWT_SECRET!!, (error: any, user: any) => {
       if (error) res.status(403).json("Token is not valid!");
       req.user = user;
       next();
